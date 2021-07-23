420fb0c3232877ec82923ab154ba74e8af4ad82b
ThibertChristophe <c.thibert89@gmail.com>
2021-07-23 16:46:14 +0200

    First component Navigation

--- facebook-clone/src/App.js
+++ facebook-clone/src/App.js
@@ -1,15 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Home />
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/about" exact component={About}></Route>
+        <Route component={Home}></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
