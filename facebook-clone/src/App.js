6f4fbc7a2337372eb14e89360fb87ad5ffd528ea
ThibertChristophe <c.thibert89@gmail.com>
2021-07-23 15:57:42 +0200

    First component Home

--- facebook-clone/src/App.js
+++ facebook-clone/src/App.js
@@ -1,15 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Learn React</p>
+        <Home />
       </header>
     </div>
   );
