3f097a36cac01c7fc3b412c642bf211bf056bbc5
ThibertChristophe <c.thibert89@gmail.com>
2021-07-29 19:07:41 +0200

    Essai effet du header du middle

--- facebook-clone/src/components/HeaderMiddleWidget.js
+++ facebook-clone/src/components/HeaderMiddleWidget.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const HeaderMiddleWidget = () => {
+  const [cache, setCache] = useState("headerMiddleWidget");
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset >= 20) {
+      setCache("headerMiddleWidget cache");
+    } else {
+      setCache("headerMiddleWidget");
+    }
+  });
+
   return (
-    <div className="headerMiddleWidget">
+    <div className={cache}>
       <div className="headerMiddleWidget-menu">
         <a className="headerMiddleWidget-menu-btn active">Accueil</a>
         <a className="headerMiddleWidget-menu-btn">Favoris</a>
