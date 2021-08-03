7e6e938a8f52528cc672919c8aa8f325bc5a8127
ThibertChristophe <c.thibert89@gmail.com>
2021-08-03 15:12:35 +0200

    Ajout d'effet lors du click sur btn de header

--- facebook-clone/src/components/Button_circle.js
+++ facebook-clone/src/components/Button_circle.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Button_circle = (Icon) => {
+  const [actif, setACtif] = useState("");
+
   // Ouvre la popup de menu
   function openMenu1() {
     if (Icon.add !== undefined) {
       document.querySelector(".popup").classList.toggle("showMenu");
+      if (actif !== "") {
+        setACtif("");
+      } else {
+        setACtif("active-btn-cirle");
+      }
     }
   }
   return (
     <div className="button_circle">
-      <button onClick={openMenu1}>
+      <button onClick={openMenu1} className={actif}>
         <img src={Icon.Icon} alt="" />
       </button>
     </div>
