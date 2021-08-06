7fb2a5d2d0aca0745362a90d68d98567c885f661
ThibertChristophe <c.thibert89@gmail.com>
2021-08-06 23:22:40 +0200

    Changement d'ouverture du sous menu via le menu_right et non les boutons eux meme, on gere donc l'ouverture des sous menu via le menu de droite du header dorenavant

--- facebook-clone/src/components/Button_circle.js
+++ facebook-clone/src/components/Button_circle.js
@@ -1,44 +1,11 @@
-import React, { useState } from "react";
-import PopUp from "./PopUp";
+import React from "react";
 
 const Button_circle = (props) => {
-  const [actif, setACtif] = useState("");
-  const [pop, setPop] = useState("");
-
-  /**
-   * Ouvre ou ferme la popup de menu
-   */
-  function openMenu() {
-    // Cas du bouton add
-    if (props.type === "add") {
-      // Ici on va retirer la popup
-      if (actif !== "") {
-        // Met le bouton en inactif (CSS)
-        setACtif("");
-        // Si bouton devant afficher un popup
-        setPop("");
-      } else {
-        // Met le bouton en actif (CSS)
-        setACtif("active-btn-cirle");
-        // Fait apparaitre la popup "menu"
-        // Si bouton devant afficher un popup
-        setPop(() => {
-          return (
-            <div className="btn-circle-popup">
-              <PopUp title="Créer" content="menu" />
-            </div>
-          );
-        });
-      }
-    }
-  }
-
   return (
     <div className="button_circle">
-      <button onClick={openMenu} className={actif}>
+      <button>
         <img src={props.Icon} alt="" />
       </button>
-      {pop}
     </div>
   );
 };
--- facebook-clone/src/components/Menu_Right.js
+++ facebook-clone/src/components/Menu_Right.js
@@ -1,19 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import Button_circle from "./Button_circle";
 import img_plus from "../img/plus.svg";
 import img_messenger from "../img/messenger.svg";
 import img_notif from "../img/bell.svg";
 import img_arrow from "../img/down-arrow.svg";
 import Button_profil from "./Button_profil";
+import PopUp from "./PopUp";
 
 const Menu_Right = () => {
+  const [pop, setPop] = useState("");
+
+  function openMenu() {
+    if (pop !== "") {
+      setPop("");
+    } else {
+      setPop(
+        <div>
+          <PopUp content="menu" title="Créez" />
+        </div>
+      );
+    }
+  }
+
+  function openMessenger() {
+    if (pop !== "") {
+      setPop("");
+    } else {
+      setPop(
+        <div>
+          <PopUp content="messenger" title="Messenger" />
+        </div>
+      );
+    }
+  }
+
   return (
-    <div className="menu_right">
-      <Button_profil Name="Chris" />
-      <Button_circle Icon={img_plus} type="add" />
-      <Button_circle Icon={img_messenger} type="messenger" />
-      <Button_circle Icon={img_notif} type="notif" />
-      <Button_circle Icon={img_arrow} type="compte" />
+    <div>
+      <div className="menu_right">
+        <Button_profil Name="Chris" />
+        <div onClick={openMenu}>
+          <Button_circle Icon={img_plus} type="add" />
+        </div>
+        <div onClick={openMessenger}>
+          <Button_circle Icon={img_messenger} type="messenger" />
+        </div>
+
+        <Button_circle Icon={img_notif} type="notif" />
+        <Button_circle Icon={img_arrow} type="compte" />
+      </div>
+      {/* Sous-menu ici */}
+      {pop}
     </div>
   );
 };
