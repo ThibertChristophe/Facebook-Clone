53cadd52b3e6d6bfd1349ce3eebdb89ca492d626
ThibertChristophe <c.thibert89@gmail.com>
2021-08-11 14:21:55 +0200

    Notification component OK

--- facebook-clone/src/components/PopUpNotif.js
+++ facebook-clone/src/components/PopUpNotif.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Notification from "./Notification";
-
+import barackObama from "../img/barack.JPG";
 const PopUpNotif = () => {
   return (
     <div className="popup-notif">
@@ -9,11 +9,26 @@ const PopUpNotif = () => {
         <a href="">Voir tout</a>
       </div>
       <div className="popup-notif-notif-list">
-        <Notification notif="1ere notif" history="history" />
-        <Notification notif="1ere notif" history="history" />
-        <Notification notif="1ere notif" history="history" />
-        <Notification notif="1ere notif" history="history" />
-        <Notification notif="1ere notif" history="history" />
+        <Notification
+          notif="Prénom Nom a ajouté une publication"
+          history="Il y a 20min"
+        />
+        <Notification
+          notif="Prénom Nom a une nouvelle photo"
+          history="Il y a 2 jours"
+        />
+        <Notification
+          notif="Prénom Nom a partagé la publication de Nom Prénom"
+          history="Il y a une semaine"
+        />
+        <Notification
+          notif="Prénom Nom aime votre commentaire"
+          history="Il y a une semaine"
+        />
+        <Notification
+          notif="Prénom Nom a une nouvelle photo"
+          history="Il y a 2 semaines"
+        />
       </div>
     </div>
   );
