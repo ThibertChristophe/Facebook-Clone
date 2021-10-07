4bf0259f3d2f65282256a09aec2a2c7bd78e196e
ThibertChristophe <c.thibert89@gmail.com>
2021-10-07 16:01:50 +0200

    Debut du scroll de la partie saloon

--- facebook-clone/src/components/Button_circle.js
+++ facebook-clone/src/components/Button_circle.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Button_circle = ({ Icon, withoutOver }) => {
+const Button_circle = ({ Icon, withoutOver, renverse }) => {
+  const [listofClass, setListofClass] = useState();
+
+  useEffect(() => {
+    let listC;
+    if (withoutOver === "true") {
+      listC = "withOver";
+    }
+    if (renverse) {
+      listC += " renverse";
+    }
+    setListofClass(listC);
+  });
+
   return (
     <div className="button_circle">
-      <button className={withoutOver === "true" ? "withOver" : ""}>
+      <button className={listofClass}>
         <img src={Icon} alt="" />
       </button>
     </div>
--- facebook-clone/src/components/Saloons.js
+++ facebook-clone/src/components/Saloons.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Button_circle from "./Button_circle";
 import imgArrow from "../img/next.svg";
+
 const Saloons = () => {
+  const [classlist, setClasslist] = useState("listContact");
+
+  function clickNext() {
+    setClasslist("listContact defille");
+    document.querySelector(".saloons-btn-next").classList.toggle("hide");
+    document.querySelector(".saloons-btn-previous").classList.toggle("show");
+  }
+
   return (
     <div className="saloons">
-      <div className="createSaloon">
-        <button>
-          <span className="camera"></span>Créer un salon
-        </button>
+      <div className="saloons-btn-previous">
+        <Button_circle Icon={imgArrow} withoutOver="true" renverse />
       </div>
-      <div className="listContact">
+      <div className={classlist}>
+        <div className="createSaloon">
+          <button>
+            <span className="camera"></span>Créer un salon
+          </button>
+        </div>
         <div className="img"></div>
         <div className="img"></div>
         <div className="img"></div>
+        <div className="img"></div>
+        <div className="img"></div>
+        <div className="img"></div>
+        <div className="img"></div>
+        <div className="img"></div>
+        {/* depasse*/}
         <div className="img"></div>
         <div className="img"></div>
         <div className="img"></div>
         <div className="img"></div>
         <div className="img"></div>
       </div>
-      <div className="saloons-btn-next">
+      <div className="saloons-btn-next" onClick={clickNext}>
         <Button_circle Icon={imgArrow} withoutOver="true" />
       </div>
     </div>
