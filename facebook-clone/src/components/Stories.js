29e9f1d0107c46a1be1a5b2e5d2c74ec55750f2d
ThibertChristophe <c.thibert89@gmail.com>
2021-11-02 16:12:54 +0100

    Liste des story en state pour s'adapter au responsive

--- facebook-clone/src/components/Stories.js
+++ facebook-clone/src/components/Stories.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Story from "./Story";
 import imgProfil from "../img/profil.jpg";
 import imgElon from "../img/elon_musk.JPG";
@@ -9,14 +9,43 @@ import Button_circle from "./Button_circle";
 import imgArrow from "../img/right-arrow.svg";
 
 const Stories = () => {
+  const [listStory, setListStory] = useState([]);
+
+  useEffect(() => {
+    // On créé notre liste de story
+    setListStory([]);
+    setListStory((listStory) => [
+      ...listStory,
+      { name: "Elon Musk", img: imgElon },
+    ]);
+    setListStory((listStory) => [
+      ...listStory,
+      { name: "Albert Einstein", img: imgAlbert },
+    ]);
+    setListStory((listStory) => [
+      ...listStory,
+      { name: "Bill Gates", img: imgBill },
+    ]);
+    setListStory((listStory) => [
+      ...listStory,
+      { name: "Barack Obama", img: imgBarack },
+    ]);
+    setListStory((listStory) => [...listStory, { name: "Monsieur Mystère" }]);
+  }, []);
+
+  window.addEventListener("resize", () => {
+    if (window.innerWidth < 1450 && listStory.length === 5) {
+      // setListStory(listStory.slice(0, listStory.length - 1));
+    }
+  });
+
   return (
     <div className="stories">
       <Story title="Créer une story" first mini={imgProfil} />
-      <Story title="Elon Musk" mini={imgElon} />
-      <Story title="Albert Einstein" mini={imgAlbert} />
-      <Story title="Bill Gates" mini={imgBill} />
-      <Story title="Barack Obama" mini={imgBarack} />
-      <Story title="Monsieur Mystère" mini="" />
+      {/*List de story*/}
+      {listStory.map((story, index) => (
+        <Story title={story.name} mini={story.img} key={index} />
+      ))}
       <div className="stories-btn-next">
         <Button_circle Icon={imgArrow} withoutOver="true" />
       </div>
