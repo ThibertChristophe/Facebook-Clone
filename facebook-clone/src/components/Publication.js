ca1ea3affe803cf39e8b2448058694bf54553821
ThibertChristophe <c.thibert89@gmail.com>
2021-11-09 13:26:49 +0100

    Suite de la page profil

--- facebook-clone/src/components/Publication.js
+++ facebook-clone/src/components/Publication.js
@@ -1,6 +1,9 @@
 import React from "react";
 import WhatNew from "./WhatNew";
 
+import Posts from "./Posts";
+import { NavLink } from "react-router-dom";
+
 const Publication = () => {
   return (
     <div className="publication">
@@ -34,7 +37,44 @@ const Publication = () => {
           <WhatNew />
         </div>
         <div className="box publication__left__public">
-          <h2>Publications</h2>
+          <div className="publication__left__public__head">
+            <h2>Publications</h2>
+            <div className="publication__left__public__head__listbtn">
+              <div className="btn filtre">
+                <img src="" alt="" />
+                <span>Filtres</span>
+              </div>
+              <div className="btn pubBtn">
+                <img src="" alt="" />
+                <span>Gérer les publications</span>
+              </div>
+            </div>
+          </div>
+          <div className="publication__left__public__bot">
+            <div className="switch">
+              <img src="" alt="" />
+              <NavLink to="/Profil/" exact>
+                Vue Liste
+              </NavLink>
+            </div>
+            <div className="switch">
+              <img src="" alt="" />
+              <span>Vue Grille</span>
+            </div>
+
+            <div className="publication__left__public__bot__grid"></div>
+          </div>
+        </div>
+        <div className="publication__left__postsList">
+          <div className="publication__left__postsList__post">
+            <Posts titre="Nom Prénom" />
+          </div>
+          <div className="publication__left__postsList__post">
+            <Posts titre="Nom Prénom" />
+          </div>
+          <div className="publication__left__postsList__post">
+            <Posts titre="Nom Prénom" />
+          </div>
         </div>
       </div>
     </div>
