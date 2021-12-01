cdbae078adc50bc973aec1ff204241dfd3564700
ThibertChristophe <c.thibert89@gmail.com>
2021-12-01 12:17:36 +0100

    partie request ok

--- facebook-clone/src/components/Requests.js
+++ facebook-clone/src/components/Requests.js
@@ -1,9 +1,16 @@
 import React from "react";
+import noPeople from "../img/null_states_people_dark_mode.svg";
 
 const Requests = () => {
   return (
     <div className="requests">
-      Sélectionnez des noms de personnes pour avoir un aperçu de leur profil.
+      <div className="requests__content">
+        <img src={noPeople} alt="" />
+        <div>
+          Sélectionnez des noms de personnes pour avoir un aperçu de leur
+          profil.
+        </div>
+      </div>
     </div>
   );
 };
