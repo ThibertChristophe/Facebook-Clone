9c0152306e0cfe4e86c5fe72670f550a806609aa
ThibertChristophe <c.thibert89@gmail.com>
2021-12-02 16:37:39 +0100

    debut de sidebar

--- facebook-clone/src/components/SideBarWatch.js
+++ facebook-clone/src/components/SideBarWatch.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const SideBarWatch = () => {
-  return <div className="sideBarWatch"></div>;
+  return (
+    <div className="sideBarWatch">
+      <div className="sideBarWatch__header">
+        <div className="sideBarWatch__header__titre">
+          <h2>Watch</h2>
+          <div className="ecrouFond">
+            <span className="ecrou"></span>
+          </div>
+        </div>
+        <div className="sideBarWatch__header__input"></div>
+      </div>
+    </div>
+  );
 };
 
 export default SideBarWatch;
