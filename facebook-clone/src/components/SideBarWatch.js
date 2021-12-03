911a69bb2c9e068613befc7ff279d86a84f0461e
ThibertChristophe <c.thibert89@gmail.com>
2021-12-03 15:25:26 +0100

    suite sidebarwatch

--- facebook-clone/src/components/SideBarWatch.js
+++ facebook-clone/src/components/SideBarWatch.js
@@ -12,9 +12,36 @@ const SideBarWatch = () => {
         </div>
         <div className="sideBarWatch__header__input">
           <div className="loupe"></div>
-          <input type="text" />
+          <input type="text" placeholder="Rechercher des vidéos" />
         </div>
       </div>
+      <div className="sideBarWatch__list">
+        <div className="sideBarWatch__list__card ">
+          <div className="card__left homeActive">
+            <div className="card__left__img"></div>
+          </div>
+          <div className="card__right">Accueil</div>
+        </div>
+        <div className="sideBarWatch__list__card ">
+          <div className="card__left ">
+            <div className="card__left__img"></div>
+          </div>
+          <div className="card__right">En direct</div>
+        </div>
+        <div className="sideBarWatch__list__card ">
+          <div className="card__left ">
+            <div className="card__left__img"></div>
+          </div>
+          <div className="card__right">Programmes</div>
+        </div>
+        <div className="sideBarWatch__list__card ">
+          <div className="card__left ">
+            <div className="card__left__img"></div>
+          </div>
+          <div className="card__right">Vidéos enregistrées</div>
+        </div>
+      </div>
+      <div className="sideBarWatch__abonnement"></div>
     </div>
   );
 };
