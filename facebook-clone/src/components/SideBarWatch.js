b8efa5f06be8b70ef761a1902eef992bb480495f
ThibertChristophe <c.thibert89@gmail.com>
2021-12-03 16:57:46 +0100

    Suite

--- facebook-clone/src/components/SideBarWatch.js
+++ facebook-clone/src/components/SideBarWatch.js
@@ -15,33 +15,48 @@ const SideBarWatch = () => {
           <input type="text" placeholder="Rechercher des vidéos" />
         </div>
       </div>
+      {/**================== LIST ACTION ======================== */}
       <div className="sideBarWatch__list">
-        <div className="sideBarWatch__list__card ">
+        <div className="sideBarWatch__list__card active">
           <div className="card__left homeActive">
             <div className="card__left__img"></div>
           </div>
           <div className="card__right">Accueil</div>
         </div>
         <div className="sideBarWatch__list__card ">
-          <div className="card__left ">
+          <div className="card__left directActive">
             <div className="card__left__img"></div>
           </div>
           <div className="card__right">En direct</div>
         </div>
         <div className="sideBarWatch__list__card ">
-          <div className="card__left ">
+          <div className="card__left programActive">
             <div className="card__left__img"></div>
           </div>
           <div className="card__right">Programmes</div>
         </div>
         <div className="sideBarWatch__list__card ">
-          <div className="card__left ">
+          <div className="card__left videoActive">
             <div className="card__left__img"></div>
           </div>
           <div className="card__right">Vidéos enregistrées</div>
         </div>
       </div>
-      <div className="sideBarWatch__abonnement"></div>
+      {/**================== ABONNEMENTS ======================== */}
+      <div className="sideBarWatch__abonnement">
+        <div className="sideBarWatch__abonnement__header">
+          <h3>Abonnements</h3>
+          <div className="btn-gerer">Gérer</div>
+        </div>
+        <div className="sideBarWatch__abonnement__list">
+          <div className="sideBarWatch__abonnement__list__card">
+            <div className="card__left">
+              <div className="card__left__img"></div>
+            </div>
+            <div className="card__right">Vidéos les plus récentes</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
